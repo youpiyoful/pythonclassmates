1eac635b1df0051a44b37b52528c7ab2ada285f0
youpiyoful <yoanfornari@gmail.com>
2020-12-24 12:59:51 +0100

    come  with last commit

--- home/models.py
+++ home/models.py
@@ -2,7 +2,8 @@ from django.db import models
 
 from wagtail.core.models import Page
 from wagtail.core.fields import RichTextField
-from wagtail.admin.edit_handlers import FieldPanel
+from wagtail.admin.edit_handlers import FieldPanel, PageChooserPanel
+from wagtail.images.edit_handlers import ImageChooserPanel
 
 
 class HomePage(Page):
@@ -10,13 +11,29 @@ class HomePage(Page):
 
     # templates = ""
     # max_count = 3
-    body = RichTextField(blank=True)
     banner_title = models.CharField(max_length=100, blank=False, null=True,)
+    banner_subtitle = RichTextField(features=['bold', 'italic'])
+    banner_image = models.ForeignKey(
+        "wagtailimages.Image",
+        null=True,
+        blank=False,
+        on_delete=models.SET_NULL,
+        related_name='+',
+    )
+    banner_cta = models.ForeignKey(
+        'wagtailcore.Page',
+        null=True,
+        blank=False,
+        on_delete=models.SET_NULL,
+        related_name='+',
+    )
 
 
     content_panels = Page.content_panels + [
         FieldPanel('banner_title'),
-        FieldPanel('body', classname="full"),
+        FieldPanel('banner_subtitle'),
+        ImageChooserPanel('banner_image'),
+        PageChooserPanel('banner_cta'),
     ]
 
     class Meta:
