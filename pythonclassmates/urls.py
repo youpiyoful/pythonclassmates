2926f0cd5c79447beebf441df16ec317bf6853b0
youpiyoful <yoanfornari@gmail.com>
2020-12-24 13:19:04 +0100

    :wrench: installation of debug_toolbar
    need to add installend_apps debug_toolba, middleware debug_toolbar.middleware.DebugToolbarMiddleware, and add internal_ips localhost

--- pythonclassmates/urls.py
+++ pythonclassmates/urls.py
@@ -1,3 +1,4 @@
+import debug_toolbar
 from django.conf import settings
 from django.urls import include, path
 from django.contrib import admin
@@ -26,6 +27,7 @@ if settings.DEBUG:
     # Serve static and media files from development server
     urlpatterns += staticfiles_urlpatterns()
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += [path('__debug__/', include(debug_toolbar.urls)),]
 
 urlpatterns = urlpatterns + [
     # For anything not caught by a more specific rule above, hand over to
