eb61f768f09af958e7a88029bcfa84482c733945
youpiyoful <yoanfornari@gmail.com>
2020-12-30 02:00:32 +0100

    :construction: mvp for blog and events
    j'ai créé une structure minimal pour pouvoir utiliser les pages events, event, blog, article

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,34 @@
+"""The blog page models."""
 from django.db import models
 
-# Create your models here.
+from wagtail.core.models import Page
+from wagtail.core.fields import RichTextField
+from wagtail.admin.edit_handlers import FieldPanel
+
+class BlogPage(Page):
+    """Blog page class."""
+
+    description = models.CharField(max_length=255, blank=True,)
+    # TODO card list 
+
+    content_panels = Page.content_panels + [
+        FieldPanel('description', classname="full")
+    ]
+
+    class Meta: #noqa
+        verbose_name = "Blog Page"
+        verbose_name_plural = "Blog Pages"
+
+class PostPage(Page):
+    """PostPage is the page of specific articles"""
+    body = RichTextField(blank=True)
+    # table_of_recent_article
+    # table_of_contents
+
+    content_panels = Page.content_panels + [
+       FieldPanel('body', classname='full'), 
+    ]
+
+    class Meta: #noqa
+        verbose_name = "Post Page"
+        verbose_name_plural = "Post Pages"
--- events/models.py
+++ events/models.py
@@ -1,3 +1,35 @@
+"""The events page models."""
 from django.db import models
 
-# Create your models here.
+from wagtail.core.models import Page
+from wagtail.core.fields import RichTextField
+from wagtail.admin.edit_handlers import FieldPanel
+
+class EventsPage(Page):
+    """Events page class."""
+
+    description = models.CharField(max_length=255, blank=True,)
+    # TODO card list 
+
+    content_panels = Page.content_panels + [
+        FieldPanel('description', classname="full")
+    ]
+
+    class Meta: #noqa
+        verbose_name = "Event Page"
+        verbose_name_plural = "Events Pages"
+
+class EventPage(Page):
+    """EventPage is the class of specific event"""
+    body = RichTextField(blank=True)
+    # TODO
+    # inscription
+    # date
+
+    content_panels = Page.content_panels + [
+       FieldPanel('body', classname='full'), 
+    ]
+
+    class Meta: #noqa
+        verbose_name = "Event Page"
+        verbose_name_plural = "Events Pages"
