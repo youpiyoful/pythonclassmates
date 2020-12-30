f3ed15d7fdbeb9bdc45e5f071a2e652bfef3f985
youpiyoful <yoanfornari@gmail.com>
2020-12-30 03:41:23 +0100

    :wastebasket: clean the page

--- blog/models.py
+++ blog/models.py
@@ -1,7 +1,6 @@
 """The blog page models."""
 from django.db import models
 from django import forms
-from modelcluster.models import ClusterableModel
 
 from wagtail.core.models import Page
 from wagtail.core.fields import RichTextField
@@ -9,7 +8,6 @@ from wagtail.admin.edit_handlers import FieldPanel
 
 from modelcluster.fields import ParentalManyToManyField
 from modelcluster.tags import ClusterTaggableManager
-# from streams.models import Category
 
 class BlogPage(Page):
     """Blog page class."""
@@ -42,18 +40,3 @@ class PostPage(Page):
     class Meta: #noqa
         verbose_name = "Post Page"
         verbose_name_plural = "Post Pages"
-
-# @register_snippet
-# class Category(models.Model):
-#     """Category for all the website creation"""
-#     name = models.CharField(max_length=255)
-#     slug = models.SlugField(unique=True, max_length=80)
-
-#     panels = [
-#         FieldPanel('name'),
-#         FieldPanel('slug'),
-#     ]
-
-#     class Meta: # noqa
-#         verbose_name = "Category"
-#         verbose_name_plural = "Categories"
