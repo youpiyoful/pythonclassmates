d1d7a2e74d76dc893cf4e7438ee4d1b5c81a84a7
youpiyoful <yoanfornari@gmail.com>
2021-01-07 21:02:31 +0100

    :wrench: add a secret key

--- pythonclassmates/settings/travis.py
+++ pythonclassmates/settings/travis.py
@@ -1,5 +1,6 @@
 from .base import *
 
+SECRET_KEY = '{\x0f\x94~\xa1\x012\x1d\xff\xbaYN\xc87!\xd3Z(\xd3E\x9f\xfbi\xe2'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
