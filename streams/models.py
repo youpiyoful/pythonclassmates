8b7c65b72490684613e80cccb3adb2cbe3653680
youpiyoful <yoanfornari@gmail.com>
2021-01-09 20:16:52 +0100

    :sparkles: create author for snippets will be add to article, cours, and event

--- streams/models.py
+++ streams/models.py
@@ -2,9 +2,11 @@
 from django.db import models
 from taggit.models import TaggedItemBase, Tag as TaggitTag
 
-from wagtail.admin.edit_handlers import FieldPanel
+from wagtail.admin.edit_handlers import FieldPanel, MultiFieldPanel
 from modelcluster.fields import ParentalKey
 from wagtail.snippets.models import register_snippet
+from wagtail.images.edit_handlers import ImageChooserPanel
+
 
 @register_snippet
 class Category(models.Model):
@@ -34,3 +36,45 @@ class PageTag(TaggedItemBase):
 class Tag(TaggitTag): # noqa
     class Meta: # noqa
         proxy = True
+
+
+@register_snippet
+class Author(models.Model):
+    """Author for snippets."""
+
+    name = models.CharField(max_length=100)
+    website = models.URLField(blank=True, null=True)
+    
+    # image = models.ForeignKey(
+    #     "wagtailimages.Image",
+    #     on_delete=models.SET_NULL,
+    #     null=True,
+    #     blank=False,
+    #     related_name="+",
+    # )
+
+    panels = [
+        MultiFieldPanel(
+            [
+                FieldPanel("name"),
+                # Use an ImageChooserPanel because wagtailimages.Image (image property) 
+                # is a ForeignKey to an Image
+                # ImageChooserPanel("image"),
+            ],
+            heading="Name and Image",
+        ),
+        MultiFieldPanel(
+            [
+                FieldPanel("website"),
+            ],
+            heading="Links"
+        )
+    ]
+
+    def __str__(self):
+        """String repr of this class."""
+        return self.name
+
+    class Meta:  # noqa
+        verbose_name = "Blog Author"
+        verbose_name_plural = "Blog Authors"
