7dc3c38e47403d326fef63f930994bf2745dae8d
youpiyoful <yoanfornari@gmail.com>
2021-01-10 15:04:16 +0100

    :construction: migrations, new field, new classes and context
    add get_context for events, create new migrations about EventsAuthor, add field start, end, categories and tag to eventPage class

--- events/models.py
+++ events/models.py
@@ -1,9 +1,34 @@
 """The events page models."""
 from django.db import models
+from django import forms
+from django.utils import timezone
+# import datetime
 
-from wagtail.core.models import Page
+from taggit.models import TaggedItemBase
+from wagtail.core.models import Orderable, Page
 from wagtail.core.fields import RichTextField
-from wagtail.admin.edit_handlers import FieldPanel
+from wagtail.admin.edit_handlers import FieldPanel, InlinePanel, MultiFieldPanel
+from wagtail.snippets.edit_handlers import SnippetChooserPanel
+from modelcluster.fields import ParentalKey, ParentalManyToManyField
+from modelcluster.tags import ClusterTaggableManager
+
+
+
+# from resources.models import ResourcePage, ResourcesPage
+
+# class EventPageManager(PageManager):
+#     """ Custom manager for Event pages """
+
+# class EventPageQuerySet(PageQuerySet):
+#     def future_start(self):
+#         today = timezone.localtime(timezone.now()).date()
+#         return self.filter(start__gte=today)
+    
+#     def future_end(self):
+#         future_start = self.future_start()
+#         return self.filter(end__gte=future_start)
+    
+# EventPageManager = PageManager.from_queryset(EventPageQuerySet)
 
 class EventsPage(Page):
     """Events page class."""
@@ -20,6 +45,35 @@ class EventsPage(Page):
         FieldPanel('description', classname="full")
     ]
 
+    def get_context(self, request, *args, **kwargs):
+        """Adding custom stuff to our context."""
+        context = super().get_context(request, *args, **kwargs)
+        event_page = EventPage.objects.live().public()
+        print('event_page =====> ', event_page)
+        context["events"] = event_page
+        return context
+
+    # def send_data_to_resources_page(self):
+    #     """
+    #     copy the data found in the events table
+    #     and the sends in the resource tables
+    #     """
+    #     # TODO
+    #     today = timezone.localtime(timezone.now()).date()
+    #     # today = datetime.date.today()
+    #     events_past = EventPage.objects.all().filter(end__lt=today)
+    #     for event_past in events_past:
+    #         resource, created = ResourcePage.objects.get_or_create(
+    #             title=event_past.title,
+    #             description=event_past.description,
+    #
+    #         )
+
+    def delete_past_events(self):
+        """Removes past events from the events table."""
+        #TODO
+        pass
+
     class Meta: #noqa
         verbose_name = "Events Page"
         verbose_name_plural = "Events Pages"
@@ -30,14 +84,52 @@ class EventPage(Page):
     parent_page_types = ['EventsPage']
 
     body = RichTextField(blank=True)
+
+    start = models.DateTimeField("start", null=True)
+    end = models.DateTimeField('end', null=True)
+
+    categories = ParentalManyToManyField('streams.Category', blank=True)
+    tags = ClusterTaggableManager(through='events.EventPageTag', blank=True)
+
     # TODO
+    # Empêcher que end soit antérieur à start à l'enregistrement d'une page
     # inscription
     # date
 
+
     content_panels = Page.content_panels + [
-       FieldPanel('body', classname='full'), 
+        FieldPanel('body', classname='full'),
+        FieldPanel('start'),
+        FieldPanel('end'),
+        FieldPanel('categories', widget=forms.CheckboxSelectMultiple),
+        FieldPanel('tags'),
+        MultiFieldPanel(
+            [
+                InlinePanel("authors", label="Author", min_num=1, max_num=4)
+            ],
+            heading="Author(s)"
+        ),
     ]
 
     class Meta: #noqa
         verbose_name = "Event Page"
         verbose_name_plural = "Event Pages"
+
+
+class EventAuthorsOrderable(Orderable):
+    """This allows us to select one or more event authors from Snippets."""
+
+    page = ParentalKey("events.EventPage", related_name="authors")
+    author = models.ForeignKey(
+        "streams.Author",
+        on_delete=models.CASCADE,
+    )
+
+    panels = [
+    	# Use a SnippetChooserPanel because streams.Author is registered as a snippet
+        SnippetChooserPanel("author"),
+    ]
+
+class EventPageTag(TaggedItemBase):
+    """tag for events page"""
+    content_object = ParentalKey('events.EventPage', related_name='event_tags')
