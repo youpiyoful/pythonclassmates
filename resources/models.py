f0e7d715e1e50e1e57f3cd035719b4445e8498c0
youpiyoful <yoanfornari@gmail.com>
2021-01-10 20:25:00 +0100

    :construction: init class/table and template
    mvp for resourcesPage and resourcePage display

--- resources/models.py
+++ resources/models.py
@@ -1,3 +1,22 @@
+"""Page containing ResourcesPage and ResourcePage classes"""
 from django.db import models
+from wagtail.core.models import Page
 
-# Create your models here.
+class ResourcesPage(Page):
+    """List all the resources of site"""
+    # title
+    # description
+
+
+class ResourcePage(Page):
+    """Detail of specific resource"""
+    # description
+    # link
+    # title
+    # publication_date
+    # categories
+    # tags
+    # author
+
+    
+
