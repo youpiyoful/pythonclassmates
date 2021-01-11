6e9b28e98f0b14f529602d68b0a64392fa67c72b
youpiyoful <yoanfornari@gmail.com>
2021-01-11 20:27:43 +0100

    just some comment code

--- events/tests_integration.py
+++ events/tests_integration.py
@@ -1,10 +1,13 @@
 """Integration tests of the blog app pages."""
-from wagtail.images.blocks import ImageChooserBlock
+# from wagtail.images.blocks import ImageChooserBlock
+from wagtail.core.models import Page
 from wagtail.tests.utils import WagtailPageTests
 from wagtail.tests.utils.form_data import nested_form_data, streamfield
 from events.models import EventPage, EventsPage
 from home.models import HomePage
 from blog.models import BlogPage, PostPage
+from events.management.commands.copy_and_delete_past_event import Command
+import datetime
 # from flex.models import FlexPage
 # from resources import ResourcesPage
 
@@ -32,6 +35,21 @@ class EventsPageTests(WagtailPageTests):
 
 class EventPageTests(WagtailPageTests):
     """Event page behavior test."""
+    # fixtures = ['data.json']
+    # @classmethod
+    # def setUpTestData(cls):
+    #     """Init the db for the tests of class"""
+    #     events_page = EventsPage.objects.create(
+    #         page_ptr_id=1,
+    #         description="page des évènements",
+    #         path="000001111222",
+    #         depth=3,
+    #         title="page d'évènements",
+    #         slug="events-page",
+    #     )
+    #     print("id events_page =======> ", events_page.id)
+
+
     def test_can_not_create_any_page(self):
         """
         we are testing that no child page 
@@ -55,17 +73,29 @@ class EventPageTests(WagtailPageTests):
             EventPage, {EventsPage}
         )
 
+    # def setUp(self):
+    #     EventsPage.objects.create(
+    #         description="page des évènements",
+    #         path="000001111222",
+    #         depth=3,
+    #         title="page d'évènements",
+    #         slug="events-page",
+    #     )
+    # def setUp(self):
+    #     # Test definitions as before.
+    #     call_setup_methods()
+
     # def test_can_create_event_page(self):
     #     """ Test EventPage creation are ok"""
-    #     events_page = EventsPage.objects.get(pk=8)
-        # Assert that a ContentPage can be made here, with this POST data
-        # self.assertCanCreate(events_page, EventPage, nested_form_data({
-        #     'title': 'First Event',
-        #     'body': streamfield([
-        #         ('text', 'Lorem ipsum dolor sit amet'),
-        #     ])
-            # 'blog_image': ImageChooserBlock
-        # }))
+    #     events_page = EventsPage.objects.all()[0]
+    #     # events_page = cls.events_page
+    #     # Assert that a ContentPage can be made here, with this POST data
+    #     self.assertCanCreate(events_page, EventPage, nested_form_data({
+    #         'title': 'First Event',
+    #         'body': streamfield([
+    #             ('text', 'Lorem ipsum dolor sit amet'),
+    #         ])
+    # }))
         # custom_title
         # blog_image
         # description
@@ -73,3 +103,48 @@ class EventPageTests(WagtailPageTests):
         # categories
         # tags
         # content_panels
+
+# class CommandTest(WagtailPageTests):
+#     """
+#     Test the custom django command which copies past
+#     events to resources before deleting them.
+#     """
+    # fixtures = ['data.json']
+    # def setUp(self):
+    #     """create event past"""
+        
+        # home_page = HomePage(
+        #     title="home page",
+        #     slug="home-page",
+        #     banner_title="banner",
+        #     banner_subtitle=""
+        # )
+            
+        # )
+
+        # child_page = EventsPage(
+        #     title="page parent",
+        #     slug="parent-page",
+        # )
+        # home_page.add_child(child_page)
+        # self.events_past = EventsPage.objects.all()[0]
+        # past_date = datetime.datetime.now() - datetime.timedelta(days=1)
+        # self.event_past = EventPage.objects.create(
+        #     title="Past event",
+        #     slug="past-event",
+        #     start=past_date,
+        #     end=past_date,
+        #     depth=4,
+        #     path='0001000100020001',
+        # )
+        # Page.objects.create(id=)
+        # self.events_past = EventsPage.add_child(instance=event_past)
+        # self.events_past.add_child(instance=event_past)
+
+    # def test_event_past_is_successfully_copy_and_delete(self):
+    #     """ Test than event past is copy to resources and delete from events"""
+        
+    #     # Command.handle()
+    #     # self.assertIsNone(self.events_past)
+    #     home_page = HomePage.objects.all()[0]
+    #     self.assertEqual(home_page.title, "Home")
