ab22678b04d87a3590e109fec10cc00142ed5974
youpiyoful <yoanfornari@gmail.com>
2021-01-12 19:34:01 +0100

    :construction: add context and new field to ResourcesPage
    add field description to class ResourcesPage and add context all resources

--- resources/models.py
+++ resources/models.py
@@ -1,5 +1,6 @@
 """Page containing ResourcesPage and ResourcePage classes"""
 from django.db import models
+from wagtail.admin.edit_handlers import FieldPanel
 from wagtail.core.models import Page
 
 class ResourcesPage(Page):
@@ -9,8 +10,19 @@ class ResourcesPage(Page):
     ]
     parent_page_types = ['home.HomePage']
     # title
-    # description
+    description = models.CharField(max_length=255, blank=True,)
 
+    content_panels = Page.content_panels + [
+        FieldPanel('description', classname='full'),
+    ]
+
+
+    def get_context(self, request, *args, **kwargs):
+        """Addind custom stuff to our context"""
+        context = super().get_context(request, *args, **kwargs)
+        resources = ResourcePage.objects.live().public()
+        context["resources"] = resources
+        return context
 
 class ResourcePage(Page):
     """Detail of specific resource"""
