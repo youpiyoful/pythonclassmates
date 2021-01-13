825907d17d37578defe42d74e3e50b2cc1bcba76
youpiyoful <yoanfornari@gmail.com>
2021-01-13 03:43:10 +0100

    :sparkles: add auditor to modelAdmin

--- auditor/admin.py
+++ auditor/admin.py
@@ -1,3 +1,21 @@
-from django.contrib import admin
+ 
+from wagtail.contrib.modeladmin.options import (
+    ModelAdmin,
+    modeladmin_register,
+)
+from .models import Auditors
 
-# Register your models here.
+
+class AuditorAdmin(ModelAdmin):
+    """Auditor admin."""
+
+    model = Auditors
+    menu_label = "Auditors"
+    menu_icon = "placeholder"
+    menu_order = 290
+    add_to_settings_menu = False
+    exclude_from_explorer = False
+    list_display = ("email", "full_name",)
+    search_fields = ("email", "full_name",)
+
+modeladmin_register(AuditorAdmin)
